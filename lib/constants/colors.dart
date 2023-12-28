0ed9dd2ca2dbd5a9968d78eea3cc099edd688b47
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-28 13:03:21 +0530

    add login screen

---
+++ lib/constants/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color KbgBlack = Color(0xff121212);
--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/constants/colors.dart';
 import 'package:firebase/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -12,23 +13,91 @@ class _LoginState extends State<Login> {
   //ref for the class
   final AuthServices _auth = AuthServices();
 
+  //form key
+  final _formkey = GlobalKey<FormState>();
+
+  //email password state
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: KbgBlack,
       appBar: AppBar(
-        title: Text("Sign In"),
+        backgroundColor: KbgBlack,
+        title: Text(
+          "Sign In",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: ElevatedButton(
-        onPressed: () async {
-          dynamic result = await _auth.signInAnonymous();
-          if (result == Null) {
-            print("Error signin");
-          } else {
-            print("sign in anonymous");
-            print(result?.uid);
-          }
-        },
-        child: const Text("Sign in Anonymously"),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
+          child: Column(
+            children: [
+              Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (val) =>
+                            val?.isEmpty == true ? "Enter e valid email" : null,
+                        onChanged: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        validator: (val) =>
+                            val!.length < 6 ? "Enter a valid password" : null,
+                        onChanged: (val) {
+                          setState(() {
+                            password = val;
+                          });
+                        },
+                      )
+                    ],
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "REGISTER",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("LOGIN"),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    dynamic result = await _auth.signInAnonymous();
+                    if (result == Null) {
+                      print("Error signin");
+                    } else {
+                      print("sign in anonymous");
+                      print(result?.uid);
+                    }
+                  },
+                  child: const Text("Sign in Anonymously"),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
